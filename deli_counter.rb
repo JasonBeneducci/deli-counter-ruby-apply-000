0686e3c4ab97a05a05ddcf6fb1a9199f25588021
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-06-10 23:55:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,17 @@
 # Write your code here.
 def take_a_number(name)
-line = 0
-beneducci_deli = []
 
-name.each do {|name|}
-beneducci_deli << name 
+beneducci_deli_customers = []
+count = 0
+
+while name.each do {|name|}
+beneducci_deli << name
+count +=1
 
 end
 
 return beneducci_deli
-puts ("Hello" + name + "you are number ")
+puts ("Hello" + name + "you are number" + count)
 
 
 
