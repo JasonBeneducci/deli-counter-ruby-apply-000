4cfc0e40450a83263294bf0532b09f76a35f26a5
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-06-10 23:59:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,15 @@ def take_a_number(name)
 beneducci_deli_customers = []
 count = 0
 
-while name.each do {|name|}
+
+name.each do {|name|}
 beneducci_deli << name
 count +=1
-
 end
 
-return beneducci_deli
-puts ("Hello" + name + "you are number" + count)
-
-
-
+if count == 0 
+  puts "The line is currently empty"
+else
+  puts ("Hello" + name + "you are number" + count.)
+end
 end
