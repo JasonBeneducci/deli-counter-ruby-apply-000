c256cdc549749fdf5f22650915382092cc0372c3
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-06-10 23:51:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def take_a_number(name)
+line = 0
+beneducci_deli = []
+
+name.each do {|name|}
+beneducci_deli << name 
+
+end
+
+return beneducci_deli
+puts ("Hello" + name + "you are number ")
+
+
+
+end
